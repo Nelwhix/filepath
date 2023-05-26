0d890f92d5d383e18858b25af31cf5d888ead18c
Nelson Isioma <58360242+Nelwhix@users.noreply.github.com>
2023-05-26 08:07:36 +0100

    Ãadded more methods and tests

--- src/Filepath.php
+++ src/Filepath.php
@@ -3,7 +3,7 @@
 namespace Nelwhix\Filepath;
 
 class Filepath {
-    public static function Join(string ...$elements): string {
+    public static function join(string ...$elements): string {
         $formattedPath = "";
         foreach ($elements as $element) {
             $formattedPath = $formattedPath . DIRECTORY_SEPARATOR . $element;
@@ -12,13 +12,13 @@ class Filepath {
         return $formattedPath;
     }
 
-    public static function Base(string $path): string {
+    public static function base(string $path): string {
         if (strlen($path) === 0) return ".";
         $lastIndex = strrpos($path, "/");
 
-//        if ($lastIndex === strlen($path) - 1) {
-//            $lastIndex = strrpos(substr($path, 0, $lastIndex), "/");
-//        }
+        if ($lastIndex === strlen($path) - 1) {
+            $lastIndex = strrpos(substr($path, 0, $lastIndex), "/");
+        }
 
         if (!$lastIndex) {
             return $path;
@@ -26,9 +26,39 @@ class Filepath {
 
        return substr($path, $lastIndex + 1);
     }
+
+    public static function abs(string $path): string {
+        return Filepath::join(__DIR__, $path);
+    }
+
+    public static function dir(string $path): string {
+        if (strlen($path) === 0) return ".";
+        if ($path === "..") return "..";
+        $lastIndex = strrpos($path, "/");
+
+        if ($lastIndex === false) {
+            return ".";
+        }
+
+        if ($lastIndex === 0) {
+            return "/";
+        }
+
+        return rtrim(substr(Filepath::clean($path), 0, $lastIndex), "/");
+    }
+
+    public static function clean(string $dirtyPath): string {
+        return preg_replace('#/+#', '/', $dirtyPath);
+    }
+
+    public static function ext(string $path): string {
+        $base = Filepath::base($path);
+        $lIndex = strrpos($base, ".");
+
+        return substr($base, $lIndex);
+    }
 }
 
-var_dump(Filepath::Base("/foo/bar/baz/"));
 
 
 
--- tests/Unit/Filepath_Test.php
+++ tests/Unit/Filepath_Test.php
@@ -3,10 +3,11 @@
 use Nelwhix\Filepath\Filepath;
 
 it('returns the base of a path', function (string $path, string $base) {
-    expect(Filepath::Base($path))->toBe($base);
+    expect(Filepath::base($path))->toBe($base);
 })->with([
     ["/foo/bar/baz.js", "baz.js"],
     ["/foo/bar/baz", "baz"],
+    ["/foo/bar/baz/", "baz/"],
     ["dev.txt", "dev.txt"],
     ["../todo.txt", "todo.txt"],
     ["..", ".."],
@@ -14,3 +15,22 @@ it('returns the base of a path', function (string $path, string $base) {
     ["/", "/"],
     ["", "."],
 ]);
+
+it('returns the path\'s directory', function (string $path, string $dir) {
+    expect(Filepath::dir($path))->toBe($dir);
+})->with([
+    ["/foo/bar/baz.js", "/foo/bar"],
+    ["/foo/bar/baz", "/foo/bar"],
+    ["/foo/bar/baz/", "/foo/bar/baz"],
+    ["/dirty//path///", "/dirty/path"],
+    ["dev.txt", "."],
+    ["../todo.txt", ".."],
+    ["..", ".."],
+    [".", "."],
+    ["/", "/"],
+    ["", "."],
+]);
+
+it('returns the file extension from the path', function (string $path, string $ext) {
+
+});
